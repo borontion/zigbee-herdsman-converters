2859eca0e9382ed878ec3c610aa728adb6bfdb24
Pengzhan Zhao <borontion@gmail.com>
2020-05-17 06:32:45 +0800

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+cp ./devices.js /opt/zigbee2mqtt/node_modules/zigbee-herdsman-converters/devices.js
+cp ./converters/fromZigbee.js /opt/zigbee2mqtt/node_modules/zigbee-herdsman-converters/converters/fromZigbee.js
+cp ./converters/toZigbee.js /opt/zigbee2mqtt/node_modules/zigbee-herdsman-converters/converters/toZigbee.js
+cp ./converters/utils.js /opt/zigbee2mqtt/node_modules/zigbee-herdsman-converters/converters/utils.js
